9058b2e9b24f2fd6f71d1be1dad4c4e9f03b9f68
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 19:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deepestChild(){
 function increaseRankBy(n){
   var ranks = document.querySelectorAll(".ranked-list");
   console.log(ranks);
-  // ranks.forEach(function(element){
-  //   element.innerHTML = parseInt(element.innerHTML) + n;
-  // });
+  ranks.forEach(function(element){
+    element.innerHTML = parseInt(element.innerHTML) + n;
+  });
 }
