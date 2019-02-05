ada6816dce9411baf39cc1e08e68da7cb9f19698
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 19:07:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild(){
+  var node = document.querySelector("#grand-node");
+  while(node.children.length !== 0){
+    node = node.children[0];
+  }
+  return node;
+}
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll(".ranked-list");
+  for(let rank of ranks){
+    rank.innerHTML = parseInt(rank.innerHTML) + n;
+  }
+}
